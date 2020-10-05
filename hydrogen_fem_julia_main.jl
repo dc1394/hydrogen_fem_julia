8d12983c33b01cbe7de6d56f190e1d17d734b180
dc1394 <hriver2014-bitbucket@memoad.jp>
2020-10-05 11:52:18 +0900

    rcでの境界条件処理を行うようにした

--- hydrogen_fem_julia_main.jl
+++ hydrogen_fem_julia_main.jl
@@ -1,14 +1,15 @@
 include("hydrogen_fem_julia.jl")
 using Printf
+using .Hydrogen_FEM
 #using Plots
 
 function main()
-    param, val = construct()
-    eigenval = do_run(param, val)
+    param, val = Hydrogen_FEM.construct()
+    eigenval = Hydrogen_FEM.do_run(param, val)
 
     @printf "計算が終わりました: 基底状態のエネルギー固有値E = %.14f (Hartree)\n" eigenval
-#    save_result(val)
-#    @printf "計算結果を%sに書き込みました\n" param.RESULT_FILENAME
-#    plot(val.node_r_glo, val.phi)
+    Hydrogen_FEM.save_result(val)
+    @printf "計算結果を%sに書き込みました\n" param.RESULT_FILENAME
+    #plot(val.node_r_glo, val.phi)
 end
 @time main()
