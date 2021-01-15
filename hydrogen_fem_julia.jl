edbb60ed8ec45c1fb1de529745649b0bf9f07dfd
dc1394 <hriver2014-bitbucket@memoad.jp>
2021-01-15 19:41:33 +0900

    細かい修正

--- hydrogen_fem_julia.jl
+++ hydrogen_fem_julia.jl
@@ -45,7 +45,7 @@ module Hydrogen_FEM
         # 固有ベクトルの要素数を増やす
         resize!(val.phi, NODE_TOTAL)
 
-        # 端点を0にする
+        # 端点rcでの値を0にする
         val.phi[NODE_TOTAL] = 0.0
 
         # 固有ベクトル（波動関数）を規格化
@@ -186,6 +186,6 @@ module Hydrogen_FEM
             sum += (f0 + 4.0 * f1 + f2)
         end
         
-        val.phi = map(x -> abs(x) / sqrt(sum * val.length[1] / 3.0), val.phi)
+        val.phi = map(x -> abs(x / sqrt(sum * val.length[1] / 3.0)), val.phi)
     end
 end
